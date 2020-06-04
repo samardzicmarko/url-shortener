f6fd3bafd8d69049b234eeb48b12f8bb9f23d641
samardzicmarko <samardzicmarko3@gmail.com>
2020-06-04 03:57:24 +0200

    a

--- src/main/java/com/rest/url_shortener/LoadDb.java
+++ src/main/java/com/rest/url_shortener/LoadDb.java
@@ -1,12 +1,9 @@
 package com.rest.url_shortener;
 import lombok.extern.slf4j.Slf4j;
 
-import org.apache.catalina.servlets.WebdavServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.ServletRegistration;
 
 @Configuration
 @Slf4j
